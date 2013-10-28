649bbfb5d7ae17ee34b00ad8203bef49f1030afb
Jake <jake_cohen@me.com>
2013-10-28 16:03:48 -0700

    created tile highlight script
    with various highlight prefabs

---
+++ Assets/Scripts/HighlightPulse.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighlightPulse : MonoBehaviour {
+	
+	private float lowAlpha;
+	private float highAlpha;
+	private bool increasing;
+	
+	
+	void Awake(){
+		lowAlpha=.1f;
+		highAlpha=1.0f;
+		Color color = transform.renderer.material.color;
+		color.a = highAlpha;
+		transform.renderer.material.color = color;
+		increasing=false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Color color =  transform.renderer.material.color;
+		if(increasing){
+			color.a += .7f*Time.deltaTime;
+			transform.renderer.material.color = color;
+			if(transform.renderer.material.color.a >= highAlpha) increasing = false;
+		}else{
+			color.a -= .7f*Time.deltaTime;
+			transform.renderer.material.color = color;
+			if(transform.renderer.material.color.a <= lowAlpha) increasing = true;
+		}
+	}
+}
+
